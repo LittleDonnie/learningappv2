ad4b9f46146bfdfe2fc20a9c08bdb1cb5cd0cbf5
Monty Montgomery <poweredbyplants@gmail.com>
2021-08-15 16:15:12 -0500

    Initial Commit

---
+++ LearningApp v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningApp v2
+//
+//  Created by Monty Montgomery on 8/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp v2/LearningApp_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp_v2App.swift
+//  LearningApp v2
+//
+//  Created by Monty Montgomery on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
